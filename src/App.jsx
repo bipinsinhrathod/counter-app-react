0a5ea52c93efa511232d4925afd72f9413361a84
bipinsinhrathod <bipinrajput548@gmail.com>
2024-04-12 18:44:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+// 
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [count, setCount] = useState(0)
+
+  const add = () => {
+      setCount(count + 1)
+  }
+
+  const remove = () => {
+    // Check if count is greater than 0 before decrementing
+    if (count > 0) {
+      setCount(count - 1)
+    }
+  }
+
+  return (
+    <>
+      <h1>This is the counter App</h1>
+
+      <h3>Counter: {count}</h3>
+
+      <button onClick={add}>Add</button>
+
+      <br />
+      <br />
+
+      <button onClick={remove}>Remove</button>
+    </>
+  )
+}
+
+export default App
